db8559f024fd1320847156a737601a7ce93fd820
Irshad-creates <ansariirshad78601@gmail.com>
2026-08-20 17:32:50 +0530

    Refactor code structure for improved readability and maintainability

---
+++ cpp STl/basics.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//void function - doesnt return anything 
+void print(){
+    cout << "hello";
+}
+
+// return function -  returns a value to main
+int sum(int n1, int n2){
+    return n1+n2;
+}
+
+// b0asic skeleton
+int main(){
+    
+    
+    print();
+
+    int s;
+    sum(1,2);
+    cout << sum;
+    return 0;
+}
+
